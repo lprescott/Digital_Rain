97c032d97b41478d966eebf40aac58400603ddc9
lprescott <lukep@nycap.rr.com>
2018-01-18 15:51:42 -0500

    Adding comments and other finishing touches.

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,18 @@
-var symbolSize = 26;
+/*
+  @author: lprescott 
+  https://github.com/lprescott/
+
+  In this javascript program, I followed the tutorial by emilyxxie:
+  https://www.youtube.com/watch?v=S1TQCi9axzg
+*/
+
+//Global variables here:
+var symbolSize = 20;
 var streams = [];
 
+/*
+  The setup() function is called once when the program starts.
+*/
 function setup() {
   createCanvas(
     window.innerWidth, 
@@ -8,30 +20,44 @@ function setup() {
   );
   background(0);
   var x = 0;
-  var y = 0;
   for (var i = 0; i <= width / symbolSize; i++) {
     var stream = new Stream();
-    stream.generateSymbols(x, y);
+    stream.generateSymbols(
+      x, 
+      random(-1000, 0)
+    );
     streams.push(stream);
     x += symbolSize;
   }
   textSize(symbolSize);
 }
 
+/*
+  Called directly after setup(), the draw() function continuously executes 
+  the lines of code contained inside its block until the program is stopped 
+  or noLoop() is called.
+*/
 function draw() {
-  background(0);
+  background(0, 150); //background(color, opacity)
   streams.forEach(function(stream) {
     stream.render();
   });
 }
 
-function Symbol(x, y, speed) {
+/*
+  The Symbol class
+*/
+function Symbol(x, y, speed, first) {
   this.x = x;
   this.y = y;
   this.value;
   this.speed = speed;
   this.switchInterval = round(random(2, 20));
+  this.first = first;
 
+  /*
+    The setToRandomSymbol function
+  */
   this.setToRandomSymbol = function() {
     if (frameCount % this.switchInterval == 0) {
       this.value = String.fromCharCode(
@@ -40,6 +66,9 @@ function Symbol(x, y, speed) {
     }
   }
 
+  /*
+    The rain function
+  */
   this.rain = function() {
     if (this.y >= height) {
       this.y = 0;
@@ -49,23 +78,42 @@ function Symbol(x, y, speed) {
   }
 }
 
+/*
+  The Stream class
+*/
 function Stream(){
   this.symbols = [];
   this.totalSymbols = round(random(5, 30));
-  this.speed = random(1, 15);
+  this.speed = random(5, 20);
 
+  /*
+    The generateSymbols function
+  */
   this.generateSymbols = function(x, y) {
+    var first = round(random(0, 4)) == 1; //Randomly true or false
     for (var i = 0; i <= this.totalSymbols; i++) {
-      symbol = new Symbol(x, y, this.speed);
+      symbol = new Symbol(x, y, this.speed, first);
       symbol.setToRandomSymbol();
       this.symbols.push(symbol);
       y -= symbolSize;
+      
+      //Second and onward call of this function is not first, so set to false
+      first = false; 
     }
   }
 
+  /*
+    The render function
+  */
   this.render = function() {
     this.symbols.forEach(function(symbol) {
-      fill(0, 255, 70);
+      //If it is the first symbol, brighten the fill color
+      if (symbol.first) {
+        fill(180, 255, 180);
+      } else { //keep color normally green
+        fill(30, 197, 3);
+      }
+      
       text(symbol.value, symbol.x, symbol.y);
       symbol.rain();
       symbol.setToRandomSymbol();
