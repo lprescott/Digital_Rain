b0c6d69fa03b9934e02062d9e33b85512f0bb989
lprescott <lukep@nycap.rr.com>
2018-01-18 16:07:42 -0500

    Added a fading mechanism.

--- js/sketch.js
+++ js/sketch.js
@@ -7,8 +7,9 @@
 */
 
 //Global variables here:
-var symbolSize = 20;
+var symbolSize = 14;
 var streams = [];
+var fadeInterval = 1.6;
 
 /*
   The setup() function is called once when the program starts.
@@ -19,17 +20,17 @@ function setup() {
     window.innerHeight
   );
   background(0);
+
   var x = 0;
   //Populate the entirety of the screen with new streams
   for (var i = 0; i <= width / symbolSize; i++) {
     var stream = new Stream();
-    stream.generateSymbols(
-      x, 
-      random(-1000, 0) //A random height above the screen for looks
-    );
+    stream.generateSymbols(x, random(-1000, 0));
     streams.push(stream);
     x += symbolSize;
   }
+
+  textFont('Consolas');
   textSize(symbolSize);
 }
 
@@ -48,24 +49,36 @@ function draw() {
 /*
   The Symbol class defines each 'symbol' as a character with a x, y coordinate.
   Also a speed, interval to switch and first boolean value.
+
+  Also a newly added opactiy.
 */
-function Symbol(x, y, speed, first) {
+function Symbol(x, y, speed, first, opacity) {
   this.x = x;
   this.y = y;
   this.value;
+
   this.speed = speed;
-  this.switchInterval = round(random(2, 20));
   this.first = first;
+  this.opacity = opacity;
+
+  this.switchInterval = round(random(2, 20));
 
   /*
     The setToRandomSymbol function changes a symbol class to one of the character codes
     being used, randomly.
   */
   this.setToRandomSymbol = function() {
+    var charType = round(random(0, 5));
     if (frameCount % this.switchInterval == 0) {
-      this.value = String.fromCharCode(
-        0x30A0 + round(random(0, 96))
-      );
+      if (charType > 1) {
+        // set it to Katakana
+        this.value = String.fromCharCode(
+          0x30A0 + round(random(0, 96))
+        );
+      } else {
+        // set it to numeric
+        this.value = round(random(0,9));
+      }
     }
   }
 
@@ -87,19 +100,29 @@ function Symbol(x, y, speed, first) {
 */
 function Stream(){
   this.symbols = [];
-  this.totalSymbols = round(random(5, 30));
-  this.speed = random(5, 20);
+  this.totalSymbols = round(random(5, 35));
+  this.speed = random(5, 22);
 
   /*
     The generateSymbols function sets all symbols contained in the stream to a randomly
     generated char code, using setToRandomSymbol.
   */
   this.generateSymbols = function(x, y) {
+    var opacity = 255;
     var first = round(random(0, 4)) == 1; //Randomly true or false
     for (var i = 0; i <= this.totalSymbols; i++) {
-      symbol = new Symbol(x, y, this.speed, first);
+      symbol = new Symbol(
+        x, 
+        y, 
+        this.speed, 
+        first,
+        opacity
+      );
       symbol.setToRandomSymbol();
       this.symbols.push(symbol);
+      
+      //Set the opacity to decrease as the ratio of total symbols to fade interval changes
+      opacity -= (255 / this.totalSymbols) / fadeInterval;
       y -= symbolSize;
       
       //Second and onward call of this function is not first, so set to false
@@ -114,9 +137,9 @@ function Stream(){
     this.symbols.forEach(function(symbol) {
       //If it is the first symbol, brighten the fill color
       if (symbol.first) {
-        fill(180, 255, 180);
+        fill(140, 255, 170, symbol.opacity);
       } else { //keep color normally green
-        fill(30, 197, 3);
+        fill(0, 255, 70, symbol.opacity);
       }
       
       text(symbol.value, symbol.x, symbol.y);
